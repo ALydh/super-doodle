74fdfb200c336b57283bb35f2d283f73ff7d6f52
Alexander Lydh <alexander.lydh@kognic.com>
2026-02-01 09:06:29 +0100

    refactor: extract magic numbers to Constants object
    Create Constants.scala with:
    - Validation.MaxBattlelineDuplicates (6)
    - Validation.MaxDefaultDuplicates (3)
    - Validation.MaxEnhancements (3)
    - Keywords.EpicHero
    - Defaults.UnknownDatasheet
    
    Update ArmyValidator.scala to use constants.
    
    Closes #46

---
+++ backend/src/main/scala/wahapedia/domain/Constants.scala
@@ -0,0 +1,17 @@
+package wahapedia.domain
+
+object Constants {
+  object Validation {
+    val MaxBattlelineDuplicates = 6
+    val MaxDefaultDuplicates = 3
+    val MaxEnhancements = 3
+  }
+
+  object Keywords {
+    val EpicHero = "Epic Hero"
+  }
+
+  object Defaults {
+    val UnknownDatasheet = "Unknown"
+  }
+}
